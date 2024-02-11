53dba84c05cea2768c14ad15ac770f1fde4f7a97
Mohamed Amoussaoui <mamoussa@e1r2p3.1337.ma>
2024-02-11 15:55:55 +0100

    removed the target folde from the ex00

--- ex00/target/debug/deps/ex00-11ba70c0eca1f0d1.d
+++
@@ -1,5 +0,0 @@
-/Users/mamoussa/Desktop/ready_set_boole/ex00/target/debug/deps/ex00-11ba70c0eca1f0d1: src/lib.rs
-
-/Users/mamoussa/Desktop/ready_set_boole/ex00/target/debug/deps/ex00-11ba70c0eca1f0d1.d: src/lib.rs
-
-src/lib.rs:
--- ex00/target/debug/deps/ex00-6645b1ddc492d910.d
+++
@@ -1,7 +0,0 @@
-/Users/mamoussa/Desktop/ready_set_boole/ex00/target/debug/deps/libex00-6645b1ddc492d910.rmeta: src/lib.rs
-
-/Users/mamoussa/Desktop/ready_set_boole/ex00/target/debug/deps/libex00-6645b1ddc492d910.rlib: src/lib.rs
-
-/Users/mamoussa/Desktop/ready_set_boole/ex00/target/debug/deps/ex00-6645b1ddc492d910.d: src/lib.rs
-
-src/lib.rs:
