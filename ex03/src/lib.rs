7852503a94e4ddc18f7ac6fe573e706697f95891
Mohamed Amoussaoui <mamoussa@e1r2p5.1337.ma>
2024-02-14 23:48:12 +0100

    Done adding some logical tests for and, or and xor

--- ex03/src/lib.rs
+++ ex03/src/lib.rs
@@ -26,6 +26,42 @@ mod tests {
 
     #[test]
     fn logical_and_tests() {
+        assert!(eval_formula("11&"));
+        assert!(!eval_formula("01&"));
+        assert!(!eval_formula("10&"));
+        assert!(!eval_formula("00&"));
         assert!(!eval_formula("11&1&0&1&"));
+        assert!(eval_formula("111&&"));
+        assert!(!eval_formula("111&&0&"));
+        assert!(eval_formula("1111&&&11&&11&1&1&&"));
+        assert!(!eval_formula("1111&&&10&&11&1&1&&"));
+    }
+
+    #[test]
+    fn logical_or_tests() {
+        assert!(eval_formula("11|"));
+        assert!(eval_formula("01|"));
+        assert!(eval_formula("10|"));
+        assert!(!eval_formula("00|"));
+        assert!(eval_formula("11|1|0|1|"));
+        assert!(eval_formula("111||"));
+        assert!(eval_formula("111||0|"));
+        assert!(eval_formula("1111|||11||11|1|1||"));
+        assert!(!eval_formula("0000|||00||00|0|0||"));
+        assert!(eval_formula("1111|||10||11|1|1||"));
+    }
+
+    #[test]
+    fn logical_xor_tests() {
+        assert!(eval_formula("10^"));
+        assert!(eval_formula("01^"));
+        assert!(!eval_formula("11^"));
+        assert!(!eval_formula("00^"));
+        assert!(!eval_formula("11^1^0^1^"));
+        assert!(eval_formula("111^^"));
+        assert!(eval_formula("111^^0^"));
+        assert!(!eval_formula("1111^^^11^^11^1^1^^"));
+        assert!(!eval_formula("0000^^^00^^00^0^0^^"));
+        assert!(eval_formula("1111^^^10^^11^1^1^^"));
     }
 }
