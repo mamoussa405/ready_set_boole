1013ca52ac84d794c79cd893e55b3527ab3ae81c
Mohamed Amoussaoui <mamoussa@e1r2p5.1337.ma>
2024-02-15 15:20:19 +0100

    done adding unit tests for all operators, still need to implement the not operator add its tests and add a combination  tests.

--- ex03/src/lib.rs
+++ ex03/src/lib.rs
@@ -64,4 +64,32 @@ mod tests {
         assert!(!eval_formula("0000^^^00^^00^0^0^^"));
         assert!(eval_formula("1111^^^10^^11^1^1^^"));
     }
+
+    #[test]
+    fn material_condition_tests() {
+        assert!(!eval_formula("10>"));
+        assert!(eval_formula("11>"));
+        assert!(eval_formula("01>"));
+        assert!(eval_formula("00>"));
+        assert!(eval_formula("110>>01>0>>"));
+        assert!(!eval_formula("110>>1>0>"));
+        assert!(eval_formula("1111>>>11>>11>1>1>>"));
+        assert!(!eval_formula("1111>>>11>>11>1>0>>"));
+        assert!(eval_formula("0000>>>00>>00>0>0>>"));
+        assert!(eval_formula("1111>>>10>>11>1>1>>"));
+    }
+
+    #[test]
+    fn logical_equivalence_tests() {
+        assert!(eval_formula("11="));
+        assert!(!eval_formula("01="));
+        assert!(!eval_formula("10="));
+        assert!(eval_formula("00="));
+        assert!(!eval_formula("110==01=0=="));
+        assert!(eval_formula("110==1=0="));
+        assert!(eval_formula("1111===11==11=1=1=="));
+        assert!(!eval_formula("1111===11==11=1=0=="));
+        assert!(eval_formula("0000===00==00=0=0=="));
+        assert!(!eval_formula("1111===10==11=1=1=="));
+    }
 }
