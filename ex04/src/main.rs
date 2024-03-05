a9fed39ddcc7f148171b1281c355b747456b8343
Mohamed Amoussaoui <mamoussa@e1r5p10.1337.ma>
2024-03-05 15:10:46 +0100

    Added unit tests for the sat function in lib.rs module

--- ex04/src/main.rs
+++ ex04/src/main.rs
@@ -1,6 +1,6 @@
 use ex04;
 
 fn main() {
-    ex04::print_truth_table("AB&A!B!&|");
+    ex04::print_truth_table("AB|A!B!&&AB=A==");
     // ex04::print_truth_table("");
 }
--- ex07/src/lib.rs
+++ ex07/src/lib.rs
@@ -22,4 +22,102 @@ mod tests {
         assert!(!sat("AA!&"));
         assert!(!sat("AA^"));
     }
+    
+    #[test]
+    fn satisfiable_tests() {
+        assert!(sat("A!"));
+        assert!(sat("A!A^"));
+        assert!(sat("AB="));
+        assert!(sat("AB=!"));
+        assert!(sat("AB>"));
+        assert!(sat("AB>!"));
+        assert!(sat("AB=A="));
+    }
+
+    #[test]
+    fn extended_not_satisfiable_tests() {
+        assert!(!sat("PQ|P!Q!&&"));
+        assert!(!sat("BB!="));
+        assert!(!sat("CC!&CC!|="));
+        assert!(!sat("DD|D!D!&&"));
+        assert!(!sat("EE!="));
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_empty_string_test() {
+        sat("");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_just_spaces_test() {
+        sat("      ");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_no_enough_operators_test1() {
+        sat("AAA!");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_no_enough_operators_test2() {
+        sat("A");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_no_enough_operators_test3() {
+        sat("AB");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_no_enough_operators_test4() {
+        sat("AAC|");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_no_enough_operators_test5() {
+        sat("ABCD&=>AB=|CE=F!G=>^^");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_no_enough_values_test1() {
+        sat("!");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_no_enough_values_test2() {
+        sat("!!!!!");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_unknown_symbol_test1() {
+        sat("aY=!)>K^");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_unknown_symbol_test2() {
+        sat("&&!!Abcd");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_unknown_symbol_test3() {
+        sat("1111&=>11=|11=1!0=>^");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid formula")]
+    fn invalid_formula_unknown_symbol_test4() {
+        sat("111|");
+    }
 }
